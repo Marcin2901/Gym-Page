43df79e5c8b9a35e966ce40fb4463d8cfcd2e4ed
Marcin <jamarcin2901@wp.pl>
2021-12-12 20:52:39 +0100

    Gym-Page 0.3
    Added Trainers Section, Testimonials Section with its js file, and theirs styles

---
+++ up/js/testimonials.js
@@ -0,0 +1,26 @@
+const SliderSlides = document.getElementsByClassName('testimonials__content--slider__slide');
+const indicatorImages = document.getElementsByClassName('testimonials__content--indicator__img');
+
+
+function deactivateActive() {
+    for(let img of indicatorImages) {
+       img.classList.remove('active');
+    }
+    for(let slide of SliderSlides) {
+        slide.classList.remove('active');
+    }
+}
+
+for(let img of indicatorImages) {
+    console.log(img)
+    img.addEventListener('click', () => {
+        deactivateActive()
+        img.classList.add('active')
+
+        let imgId = img.getAttribute('data-id')
+        SliderSlides[imgId].classList.add('active')
+    })
+}
+
+
+
